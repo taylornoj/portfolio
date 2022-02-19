1041d46ab763a4f1e86919c096cad00548e1015c
Taylor Noj <taylornoj@gmail.com>
2022-02-19 12:41:41 -0500

    Updates

---
+++ precache-manifest.7282f8157945ac7973dbb8fddcd4c4bd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44e690a535f65e53fcd4a7b832043cff",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ae301f634218d5386bae",
+    "url": "/portfolio/static/css/main.53ed100c.chunk.css"
+  },
+  {
+    "revision": "1d32ee3c3db43d4811cc",
+    "url": "/portfolio/static/js/2.721988ef.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.721988ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae301f634218d5386bae",
+    "url": "/portfolio/static/js/main.301a6c47.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "6c5efcc6f815b3ff9408e5f3a8efae44",
+    "url": "/portfolio/static/media/mock1.6c5efcc6.gif"
+  },
+  {
+    "revision": "84eca3b17b8370312c823428fcda2158",
+    "url": "/portfolio/static/media/mock2.84eca3b1.gif"
+  },
+  {
+    "revision": "004934457f75e404ece042b18ce4e7cb",
+    "url": "/portfolio/static/media/mock3.00493445.gif"
+  },
+  {
+    "revision": "5d6008616baea54750a719654608525f",
+    "url": "/portfolio/static/media/mock4.5d600861.gif"
+  },
+  {
+    "revision": "5f7192fb84911dba7a69bb27368b79e4",
+    "url": "/portfolio/static/media/mock5.5f7192fb.gif"
+  },
+  {
+    "revision": "cdca72ae2679a835a7bbe1dc0d856fcd",
+    "url": "/portfolio/static/media/mock6.cdca72ae.gif"
+  },
+  {
+    "revision": "87d04cbbd14dbc42f368d726ef788142",
+    "url": "/portfolio/static/media/mock7.87d04cbb.gif"
+  },
+  {
+    "revision": "f715547c768c7ba8199bb80cad542142",
+    "url": "/portfolio/static/media/mock8.f715547c.jpeg"
+  },
+  {
+    "revision": "67a567086a5e51cd876e540d3170874e",
+    "url": "/portfolio/static/media/self.67a56708.png"
+  }
+]);
