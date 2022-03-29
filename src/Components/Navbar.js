f3c7526d7f3f1c58bf3a1db0d5b67f104196d821
Taylor Noj <taylornoj@gmail.com>
2022-03-29 09:24:20 -0400

    udpate nav bar with blog link

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -12,6 +12,7 @@ export default function Navbar({ darkMode, handleClick }) {
                 <li><NavLink exact activeClassName="current" to='/'>Home</NavLink></li>
                 <li><NavLink exact activeClassName="current" to='/about'>About Me</NavLink></li>
                 <li><NavLink exact activeClassName="current" to='/portfolio'>Portfolio</NavLink></li>
+                <li><a href="https://taylorsblog.vercel.app/" exact activeClassName="current" >Blog</a></li>
                 <li><a href="https://resume.io/r/mMPy9dwPY" exact activeClassName="current" >Resume</a></li>
                 <Toggler darkMode={darkMode} handleClick={handleClick} />
             </ul>
